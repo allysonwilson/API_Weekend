ae83a73fe9589ab4e0e89a9028ef47cacc4e0dc5
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-10-20 11:47:34 +0100

    add changes

---
+++ public/app.js
@@ -0,0 +1,15 @@
+var initialize = function(){
+  var mapDiv = document.getElementById('main-map');
+  var center = {lat: 29.313  , lng:-94.7766 };
+  var MainMap = new MapWrapper(mapDiv, cneter, 10);
+  mainMap.addMarker(center);
+  mainMap.addClickEvent();
+}
+
+
+
+
+
+
+
+window.addEventListener("load", initialize);
